2f8668f84486e55dd0b34bd366c646c6978f07e9
Borna Vukovac <bvukovac@tvz.hr>
2026-04-14 18:50:41 +0200

    Popravak loga i prirema za lab 2

--- Vjezba.Model/Vjezba.Model/obj/Debug/net8.0/Vjezba.Model.AssemblyInfo.cs
+++ Vjezba.Model/Vjezba.Model/obj/Debug/net8.0/Vjezba.Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4164a00aff2996728f760fb8d89012c55758ada9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e621cfcb75b5e348ab05717f84145f96c1205d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vjezba.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
